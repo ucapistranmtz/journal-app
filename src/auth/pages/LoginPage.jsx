290d6e9ae79ff782773f7ef133edfa3317d0062f
Ulises Capistran <ucapistranmtz@gmail.com>
2023-10-06 06:52:33 -0500

    lesson 244

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,6 @@
-import { Grid, TextField, Typography } from "@mui/material"
+import { Link as RouterLink } from 'react-router-dom'
+import { Google } from "@mui/icons-material"
+import { Button, Grid, Link, TextField, Typography } from "@mui/material"
 
 
 
@@ -22,15 +24,36 @@ export const LoginPage = () => {
 
         <form>
           <Grid container>
-            <Grid item xs={12} sx={{mt:2}} >
+            <Grid item xs={12} sx={{ mt: 2 }} >
               <TextField label='email' type='email' placeholder='dev@email.com' fullWidth >
 
               </TextField>
             </Grid>
-            <Grid item xs={12} sx={{mt:2}}>
+            <Grid item xs={12} sx={{ mt: 2 }}>
               <TextField label='password' type='password' placeholder='password' fullWidth >
 
               </TextField>
+            </Grid>
+
+            <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+
+              <Grid item xs={12} sm={6}>
+                <Button variant='contained' fullWidth> Login</Button>
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+                <Button variant='contained' fullWidth>
+                  <Google />
+                  <Typography sx={{ ml: 1 }}>Google</Typography>
+                </Button>
+              </Grid>
+
+            </Grid>
+
+            <Grid container direction='row' justifyContent='end'>
+              <Link component={RouterLink} color='inherit' to="/auth/register">
+                Sign Up
+              </Link>
             </Grid>
           </Grid>
         </form>
