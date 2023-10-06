4b58ab9d459cced7e3bde04756466037654686e6
Ulises Capistran <ucapistranmtz@gmail.com>
2023-10-06 08:11:36 -0500

    lesson 246 RegisterPage.jsx

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,7 +1,63 @@
- 
+import { Link as RouterLink } from 'react-router-dom';
+import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { Google } from '@mui/icons-material';
+import { AuthLayout } from '../layout/AuthLayout';
+
 
 export const RegisterPage = () => {
   return (
-    <h1>RegisterPage</h1>
+    <AuthLayout title="Crear cuenta">
+      <form>
+          <Grid container>
+           
+            <Grid item xs={ 12 } sx={{ mt: 2 }}>
+              <TextField 
+                label="Full Name" 
+                type="text" 
+                placeholder='Full Name' 
+                fullWidth
+              />
+            </Grid>
+
+            <Grid item xs={ 12 } sx={{ mt: 2 }}>
+              <TextField 
+                label="email" 
+                type="email" 
+                placeholder='email@google.com' 
+                fullWidth
+              />
+            </Grid>
+
+            <Grid item xs={ 12 } sx={{ mt: 2 }}>
+              <TextField 
+                label="password" 
+                type="password" 
+                placeholder='Password' 
+                fullWidth
+              />
+            </Grid>
+            
+            <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
+              <Grid item xs={ 12 }>
+                <Button variant='contained' fullWidth>
+                  sign up
+                </Button>
+              </Grid>
+            </Grid>
+
+
+            <Grid container direction='row' justifyContent='end'>
+              <Typography sx={{ mr: 1 }}>Do you already have an account?</Typography>
+              <Link component={ RouterLink } color='inherit' to="/auth/login">
+                sign in 
+              </Link>
+            </Grid>
+
+          </Grid>
+
+
+        </form>
+
+    </AuthLayout>
   )
 }
