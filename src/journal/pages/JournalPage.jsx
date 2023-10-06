3cfa6213448911c5d7f630a3fc30d4a86b7e471f
Ulises Capistran <ucapistranmtz@gmail.com>
2023-10-06 06:21:48 -0500

    lesson 242

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,11 @@
 import { Typography } from '@mui/material'
- import { MailOutline} from  "@mui/icons-material"
+ 
 
 export const JournalPage = () => {
   return (
     <>
       <Typography variant='h1'>JournalPage</Typography>
-      <MailOutline/>
+  
     </>
   
   )
