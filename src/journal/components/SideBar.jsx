249e4643ab18439071963b3cbc0fb67bb7fb2437
Ulises Capistran <ucapistranmtz@gmail.com>
2023-10-10 15:08:50 -0500

    lesson 304

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,10 +1,12 @@
 import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material'
 import { TurnedInNot } from '@mui/icons-material';
 import { useSelector } from 'react-redux';
+import { SideBarItem } from './SideBarItem'
 
 export const SideBar = ({ drawerWidth = 240 }) => {
 
-    const { displayName } = useSelector( state => state.auth );
+    const { displayName } = useSelector(state => state.auth);
+    const { notes } = useSelector(state => state.journal);
 
     return (
         <Box
@@ -14,32 +16,22 @@ export const SideBar = ({ drawerWidth = 240 }) => {
             <Drawer
                 variant='permanent' // temporary
                 open
-                sx={{ 
+                sx={{
                     display: { xs: 'block' },
                     '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
                 }}
             >
                 <Toolbar>
                     <Typography variant='h6' noWrap component='div'>
-                        { displayName }
+                        {displayName}
                     </Typography>
                 </Toolbar>
                 <Divider />
 
                 <List>
                     {
-                        ['Enero','Febrero','Marzo','Abril'].map( text => (
-                            <ListItem key={ text } disablePadding>
-                                <ListItemButton>
-                                    <ListItemIcon>
-                                        <TurnedInNot />
-                                    </ListItemIcon>
-                                    <Grid container>
-                                        <ListItemText primary={ text } />
-                                        <ListItemText secondary={ 'Exercitation cillum irure elit consectetur.' } />
-                                    </Grid>
-                                </ListItemButton>
-                            </ListItem>
+                        notes.map(note => (
+                            <SideBarItem key={note.id}  {...note} />
                         ))
                     }
                 </List>
---
+++ src/journal/components/SideBarItem.jsx
@@ -0,0 +1,24 @@
+import { TurnedInNot } from '@mui/icons-material'
+import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import React, { useMemo } from 'react'
+
+export const SideBarItem = ({title='', body,id}) => {
+
+  const newTitle = useMemo ( () => {
+
+      return title.length >17 ? title.substring(0,17)+'...':title 
+  }, [title])
+  return (
+    <ListItem key={ id } disablePadding>
+    <ListItemButton>
+        <ListItemIcon>
+            <TurnedInNot />
+        </ListItemIcon>
+        <Grid container>
+            <ListItemText primary={ title } />
+            <ListItemText secondary={ body} />
+        </Grid>
+    </ListItemButton>
+</ListItem>
+  )
+}
