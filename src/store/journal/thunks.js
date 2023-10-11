3ac9d9460ee6d6217c38a8257f4ecaa46fb5c6ed
Ulises Capistran <ucapistranmtz@gmail.com>
2023-10-11 14:38:05 -0500

    lesson 319

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -16,6 +16,7 @@ export const startNewNote = () => {
         const newNote = {
             title: '',
             body: '',
+            imageUrls:[],
             date: new Date().getTime()
         }
 
